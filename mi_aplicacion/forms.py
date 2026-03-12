5c505e0ac010167190e28b0e2ea072a2576ac1f8
Luis Beltran <belmarq@hotmail.com>
2026-03-12 11:47:24 -0700

    maestros alta con crispy

--- mi_aplicacion/forms.py
+++ mi_aplicacion/forms.py
@@ -1,13 +1,37 @@
 from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field, Row, Column
 
 from mi_aplicacion.models import Escuela, Maestro
 
 class EscuelaForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('submit', 'Guardar'))
+
     class Meta:
         model = Escuela
         fields = ["nombre", "siglas"]
 
 class MaestroForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(MaestroForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column(Field('nombre'), css_class='form-group col-md-4 mb-0'),
+                Column(Field('sexo'), css_class='form-group col-md-4 mb-0'),
+                Column(Field('fecha_nacimiento'), css_class='form-group col-md-4 mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column(Field('escuela'), css_class='form-group col-md-12 mb-0'),
+                css_class='form-row'
+            ),
+            Submit('submit', 'Guardar')
+        )
+
     class Meta:
         model = Maestro
         fields = ["nombre", "escuela", "sexo", "fecha_nacimiento"]
